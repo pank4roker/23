ef77c54ce86bc27db44ad27195beff28797dbf22
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-27 09:47:02 +0300

    1 - 2

---
+++ 23_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_1
+{
+    class Program
+    {
+        enum DaysandMonth {январь,февраль,март,апрель,май,июнь,июль,август,сентябрь,октябрь,ноябрь,декабрь}
+        enum Week { Sat, Sun, Mon, Tue, Wed, Thu, Fri }
+        static void Main(string[] args)
+        {
+           //Task1
+           DaysandMonth i;
+           for (i = DaysandMonth.январь;i<=DaysandMonth.декабрь;i++)
+           {
+                Console.WriteLine($"{(int)i}\t{i}");
+           }
+            //Task2
+            Console.WriteLine();
+           Week w;
+           for(w = Week.Sat;w<=Week.Fri;w++)
+           {
+                Console.WriteLine($"{w} имеет значение {(int)w}");
+           }
+           Console.ReadKey();
+        }
+    }
+}
---
+++ 23_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("23_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("23_1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("05baf909-44ee-44f2-8be9-f3ad259db567")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
