3bbf9fbb0b559cd7eaac064b6fd2e349a1e82378
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-27 09:47:29 +0300

    3

---
+++ 23_1(3)/Func.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_1_3_
+{
+    enum Operation { Sum = 1, Sub, Mul, Div, Exit }
+    class Func
+    {
+        public void Operations(int a,int b)
+        {
+            while (true)
+            {
+                Console.WriteLine("Какое действие вы хотите сделать с переменными:\n1 - Сложение\n2 - Вычитание\n3 - Умножение\n4 - Деление\n5 - Выход");
+                int oper = Convert.ToInt32(Console.ReadLine());
+                switch ((Operation)oper)
+                {
+                    case Operation.Sum:
+                        
+                        Console.WriteLine($"\nСложение = {(a + b)}\n");
+                        break;
+                    case Operation.Sub:
+                        Console.WriteLine($"\nВычитание = {(a - b)}\n");
+                        break;
+                    case Operation.Mul:
+                        Console.WriteLine($"\nУмножение = {(a * b)}\n");
+                        break;
+                    case Operation.Div:
+                        Console.WriteLine($"\nДеление = {(a / b)}\n");
+                        break;
+                    case Operation.Exit:
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ 23_1(3)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_1_3_
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую переменную: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вторую переменную: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Func func = new Func();
+            func.Operations(a, b);
+            Console.ReadLine();
+        }
+        
+    } 
+}
+
+
