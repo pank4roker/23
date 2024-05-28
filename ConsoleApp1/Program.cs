bf81235b5480b6ec43c634f65f8de7e2a0253182
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-28 11:48:12 +0300

    Gender

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        enum Menu { Enter = 1, Conclusion, ConclusionByGender, Exit }
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                Console.WriteLine("\nВыберите пункт:");
+                Console.WriteLine("1. Ввод студентов");
+                Console.WriteLine("2. Вывод всех студентов");
+                Console.WriteLine("3. Вывод студентов по полу");
+                Console.WriteLine("4. Выход");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                Menu menuChoice = (Menu)choice;
+
+                switch (menuChoice)
+                {
+                    case Menu.Enter:
+                        Console.Write("Введите количество студентов (N): ");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < n; i++)
+                        {
+                            students.Add(Student.InPut());
+                        }
+                        break;
+
+                    case Menu.Conclusion:
+                        Console.WriteLine("Список всех студентов:");
+                        Student.OutPuts(students);
+                        break;
+
+                    case Menu.ConclusionByGender:
+                        Console.WriteLine("Список студентов по полу:");
+                        Student.OutPutGender(students);
+                        break;
+
+                    case Menu.Exit:
+                        Console.WriteLine("Выход из программы.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор. Пожалуйста, выберите снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    enum Gender {Мужской,Женский }
+    class Student
+    {
+       public string FIO { get; set; }
+       public int YearOfBirth { get; set; }
+       public Gender Genders { get; set; }
+       public Student(string fIO, int yearOfBirth, Gender genders)
+       {
+            FIO = fIO;
+            YearOfBirth = yearOfBirth;
+            Genders = genders;
+       }
+       public void OutPut()
+       {
+            Console.WriteLine($"{FIO};{YearOfBirth};{Genders}");
+       }
+        public static void OutPuts(List<Student> students) 
+        { 
+            foreach(var student in students)
+            {
+                student.OutPut();
+            }
+        }
+       public static void OutPutGender(List<Student> students) 
+       {
+            Console.WriteLine("Список студентов мужского пола:");
+            foreach (var student in students)
+            {
+                if (student.Genders == Gender.Мужской)
+                {
+                    student.OutPut();
+                }
+            }
+
+            Console.WriteLine("Список студентов женского пола:");
+            foreach (var student in students)
+            {
+                if (student.Genders == Gender.Женский)
+                {
+                    student.OutPut();
+                }
+            }
+        }
+       public static Student InPut()
+       {
+                Console.Write("Введите ФИО студента(Фамилия И.О.): ");
+                string fio = Console.ReadLine();
+                Console.Write("Введите год рождения студента: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите пол (0 мужской, 1 женский): ");
+                int genderInput = Convert.ToInt32(Console.ReadLine());
+                Gender gender = (Gender)genderInput;
+                return new Student(fio, year, gender);
+        }
+
+    }
+}
